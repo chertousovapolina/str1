33cf0bbd74e015825d7fe58b14ca7021e89f1690
chertousovapolina <115408269+chertousovapolina@users.noreply.github.com>
2022-11-20 14:34:41 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,17 @@
+﻿//Чертоусова дз задача 1
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    string a;
+    cin >> a;
+    if (a.length() % 2 == 0) { // если длина строки - четнаяб нужно вывести строку два раза
+        cout<< a << a;
+    }
+
+
+
+}
+
+
